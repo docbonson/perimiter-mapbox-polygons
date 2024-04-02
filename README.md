a1b158267619eb514ae58c1124509873de627645
Dave <docbonson@gmail.com>
2024-04-02 14:42:32 -0500

    Redid mapbox set up and moved away from React Mapbox. Render map and allow users to draw and delete polygon.

--- README.md
+++ README.md
@@ -4,9 +4,21 @@ Today I focused on integrating Mapbox with React. I successfully set up the Mapb
 
 Tomorrow, my plan is to implement the draw component, allowing users to draw, modify, and delete polygons.
 
+# Day Two
+
+Still figuring out my approach to this. Was looking into a lot of React Mapbox and React Mapbox draw. A lot of the documentation seems to be out of date will probably circle around and take a different approach.
+
+# Day Three
+
+- Drawing Controls: Provides controls for drawing polygons and deleting drawn features.
+- Event Handling: Implements callback functions to handle drawing events, such as creation, deletion, and updates.
+- Customizable Configuration: Allows for customization of drawing options and map configurations.
+- State Management: Uses React hooks to manage the state of map drawings.
+
 ## Resources
-- [React Mapbox](https://www.npmjs.com/package/react-map-gl)
+
+- [Mapbox API Reference](https://www.npmjs.com/package/react-map-gl)
+- [Mapbox Draw](https://github.com/mapbox/mapbox-gl-draw/blob/main/docs/API.md)
 - [dotenv](https://www.npmjs.com/package/dotenv)
 
 Feel free to check out the links above for more information on the tools and libraries I used.
-
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,22 +1,105 @@
-import ReactMapbox from 'react-map-gl'
+import MapboxDraw, {
+  DrawCreateEvent,
+  DrawDeleteEvent,
+  DrawEventType,
+  DrawUpdateEvent,
+  DrawFeature,
+} from "@mapbox/mapbox-gl-draw";
+import { useCallback, useEffect, useReducer, useRef } from "react";
+import mapboxgl, { Map as MapGL } from "mapbox-gl";
 
-const TOKEN = process.env.REACT_APP_API_KEY
+// Mapbox related styles
+import "mapbox-gl/dist/mapbox-gl.css";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+
+// token from mapbox
+const accessToken = process.env.REACT_APP_API_KEY;
+
+// initialize the draw plugin
+const Draw = new MapboxDraw({
+  displayControlsDefault: false,
+  controls: {
+      polygon: true,
+      trash: true
+  },
+  // Set mapbox-gl-draw to draw by default.
+  // The user does not have to click the polygon control button first.
+  defaultMode: 'draw_polygon'
+
+});
+
+type DrawingState = {
+  drawings: DrawFeature[];
+};
+
+type DrawingAction = {};
 
 const Map = () => {
-  return (
-    <div className="map-container">
-      <ReactMapbox
-        initialViewState={{
-          longitude: -91.874,
-          latitude: 42.76,
-          zoom: 5,
-        }}
-        //map styles https://docs.mapbox.com/api/maps/styles/
-        mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
-        mapboxAccessToken={TOKEN}
-      ></ReactMapbox>
-    </div>
-  )
-}
+  // ref to hold the map reference. Use this to call methods on the map when needed.
+  const mapRef = useRef<MapGL | null>(null);
+  // ref for the container html. mapbox needs this to initialize correctly
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
-export default Map
+  const [drawingState, setDrawingState] = useReducer(
+    (state: DrawingState, payload: DrawingAction) => {
+      // todo: add more state things
+      return state;
+    },
+    {
+      drawings: [],
+    }
+  );
+
+  /**
+   * Called when creating a drawing on the map.
+   */
+  const onDrawCreate = useCallback((e: DrawCreateEvent) => {
+    console.log("calling on draw create", e);
+  }, []);
+
+  /**
+   * Called when deleting a drawing on the map.
+   */
+  const onDrawDelete = useCallback((e: DrawDeleteEvent) => {
+    console.log("calling on draw delete", e);
+  }, []);
+
+  /**
+   * Called when updating a drawing on the map.
+   */
+  const onDrawUpdate = useCallback((e: DrawUpdateEvent) => {
+    console.log("calling on draw update", e);
+  }, []);
+
+  useEffect(() => {
+    // make sure to initialize the map only once
+    if (mapRef.current || !mapContainerRef.current) return;
+
+    // initialize the map
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [-91.874, 42.76],
+      zoom: 5,
+      accessToken,
+    });
+
+    // add the draw control to the map
+    mapRef.current?.addControl(Draw, "top-left");
+
+    // add event listeners for map drawing actions
+    mapRef.current?.on("draw.create" satisfies DrawEventType, onDrawCreate);
+    mapRef.current?.on("draw.delete" satisfies DrawEventType, onDrawDelete);
+    mapRef.current?.on("draw.update" satisfies DrawEventType, onDrawUpdate);
+  }, []);
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <div className="map-container" ref={mapContainerRef} />
+      <div className="section" style={{ padding: "1rem" }}>
+        <h1>Map Drawings</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Map;
